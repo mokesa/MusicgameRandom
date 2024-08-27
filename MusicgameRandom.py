9865e96f642cc839afaeb26448bdc406ce5a54d6
mokesa <139766698+mokesa@users.noreply.github.com>
2024-08-27 12:40:28 +0900

    [update] 基本形の追加
    最低限動かせるように機能を追加しました

--- MusicgameRandom.py
+++ MusicgameRandom.py
@@ -1,1 +1,18 @@
-90
+import discord
+from discord.ext import commands
+from discord import app_commands
+
+TOKEN = 'YOUR_TOKEN'
+client = discord.client()
+tree = discord.app_commands.CommandTree(client)
+
+@tree.command(name="nya", description="にゃーん")
+async def nya(ctx:discord.Intaraction):
+    await ctx.response.send_message("にゃーん")
+
+@client.event
+async def on_ready():
+    await tree.sync()
+    print("ログインしました")
+
+client.run(TOKEN)
